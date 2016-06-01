37b575f6a618b52a9738391e663a0da4105bbd0b
Daniel Ribeiro <danielrb@squareup.com>
2016-06-01 00:03:07 -0700

    logging json stream on all cases

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 XC_DESTINATION='platform=iOS Simulator,name=iPad Air,OS=9.2'
-xctool -project XCKnifeExemplar.xcodeproj -configuration Debug -derivedDataPath derivedDataPath -scheme XCKnifeExemplar -sdk iphonesimulator -destination "$XC_DESTINATION" -reporter pretty run-tests 
+xctool -project XCKnifeExemplar.xcodeproj -configuration Debug -derivedDataPath derivedDataPath -scheme XCKnifeExemplar -sdk iphonesimulator -destination "$XC_DESTINATION" -reporter pretty -reporter json-stream:xcknife-exemplar-historical-data.json-stream run-tests
