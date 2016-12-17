337e310f1ca8e71ef8701164bfbcc48b0dc68b8a
Daniel Ribeiro <danielrb@squareup.com>
2016-12-16 16:52:03 -0800

    using xcode8 + xcknife test dumper

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 XC_DESTINATION='platform=iOS Simulator,name=iPad Air,OS=9.2'
-xctool -project XCKnifeExemplar.xcodeproj -configuration Debug -derivedDataPath derivedDataPath -scheme XCKnifeExemplar -sdk iphonesimulator -destination "$XC_DESTINATION" -reporter pretty -reporter json-stream:xcknife-exemplar-historical-data.json-stream run-tests
+
+xcodebuild -project XCKnifeExemplar.xcodeproj \
+    -configuration Debug \
+    -derivedDataPath derivedDataPath \
+    -scheme XCKnifeExemplar \
+    -sdk iphonesimulator \
+    -destination "$XC_DESTINATION" \
+    test-without-building
