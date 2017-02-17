446a00c6093160b8383d22a667a570732e2c145b
Daniel Ribeiro <danielrb@squareup.com>
2017-02-17 13:56:35 -0800

    testing that env variables come from the right place

--- lib/xcknife/xcscheme_analyzer.rb
+++ lib/xcknife/xcscheme_analyzer.rb
@@ -7,11 +7,15 @@ module XCKnife
     def extract_environment_variables(xscheme_data)
       ret = {}
       xml_root = REXML::Document.new(xscheme_data).root
-      test_action = xml_root.elements["//TestAction"]
-      if test_action && test_action.attributes['shouldUseLaunchSchemeArgsEnv'] == "NO"
-        return ret
+
+
+      action = xml_root.elements["//TestAction"]
+      return ret if action.nil?
+      if action.attributes['shouldUseLaunchSchemeArgsEnv'] == "YES"
+        action = xml_root.elements["//LaunchAction"]
       end
-      env_elements = xml_root.elements["//EnvironmentVariables"]
+      return ret if action.nil?
+      env_elements = action.elements[".//EnvironmentVariables"]
       return ret if env_elements.nil?
       env_elements.elements.each do |e|
         attrs = e.attributes
--- spec/xcscheme_analyzer_spec.rb
+++ spec/xcscheme_analyzer_spec.rb
@@ -3,27 +3,37 @@ require 'spec_helper'
 describe XCKnife::XcschemeAnalyzer do
   include XCKnife::XcschemeAnalyzer
 
-  def scheme(shouldUseLaunchSchemeArgsEnv = true)
+  def environment_variables(suffix)
+<<eof
+<EnvironmentVariables>
+   <EnvironmentVariable
+      key = "ENABLED_FLAG_#{suffix}"
+      value = "1"
+      isEnabled = "YES">
+   </EnvironmentVariable>
+   <EnvironmentVariable
+      key = "DISABLED_FLAG_#{suffix}"
+      value = "does-not-matter"
+      isEnabled = "NO">
+   </EnvironmentVariable>
+</EnvironmentVariables>
+eof
+  end
+
+  def scheme(shouldUseLaunchSchemeArgsEnv)
     option_str = shouldUseLaunchSchemeArgsEnv ? "YES" : "NO"
+    test_environment_variables = environment_variables("TEST")
+    launch_environment_variables = environment_variables("LAUNCH")
     <<eof
 <?xml version="1.0" encoding="UTF-8"?>
 <Scheme
    LastUpgradeVersion = "0720"
    version = "1.3">
-   <TestAction shouldUseLaunchSchemeArgsEnv="#{option_str}"/>
+   <TestAction shouldUseLaunchSchemeArgsEnv="#{option_str}">
+    #{test_environment_variables}
+   </TestAction>
    <LaunchAction>
-      <EnvironmentVariables>
-         <EnvironmentVariable
-            key = "ENABLED_FLAG"
-            value = "1"
-            isEnabled = "YES">
-         </EnvironmentVariable>
-         <EnvironmentVariable
-            key = "DISABLED_FLAG"
-            value = "does-not-matter"
-            isEnabled = "NO">
-         </EnvironmentVariable>
-      </EnvironmentVariables>
+      #{launch_environment_variables}
       <AdditionalOptions>
       </AdditionalOptions>
    </LaunchAction>
@@ -35,15 +45,19 @@ eof
     scheme(false)
   end
 
+  def scheme_shouldUseLaunchSchemeArgsEnv_equals_to_YES
+    scheme(true)
+  end
+
 
   it "can parse xcscheme files with EnvironmentVariables" do
-    attrs = extract_environment_variables(scheme)
-    expect(attrs).to eq("ENABLED_FLAG" => "1")
+    attrs = extract_environment_variables(scheme_shouldUseLaunchSchemeArgsEnv_equals_to_YES)
+    expect(attrs).to eq("ENABLED_FLAG_LAUNCH" => "1")
   end
 
   it "ignores xcscheme EnvironmentVariables if shouldUseLaunchSchemeArgsEnv is NO" do
     attrs = extract_environment_variables(scheme_shouldUseLaunchSchemeArgsEnv_equals_to_NO)
-    expect(attrs).to eq({})
+    expect(attrs).to eq("ENABLED_FLAG_TEST" => "1")
   end
 
   it "will return empty hash if no EnvironmentVariables are listed" do
