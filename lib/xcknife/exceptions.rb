5d19f4957bfd037b9cc42dc55ba0cba9f718880e
Samuel Giddins <segiddins@squareup.com>
2018-07-03 11:09:29 -0700

    Raise exceptions when errors occur
    This makes tracking which errors are happening easier, and also makes programatic recovery when an error occurs possible

--- lib/xcknife/exceptions.rb
+++ lib/xcknife/exceptions.rb
@@ -2,5 +2,7 @@ module XCKnife
   # Base error class for xcknife
   XCKnifeError = Class.new(StandardError)
 
+  TestDumpError = Class.new(XCKnifeError)
+
   StreamParsingError = Class.new(XCKnifeError)
 end
